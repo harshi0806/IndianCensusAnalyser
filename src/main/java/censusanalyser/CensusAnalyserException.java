91a2ebbe7303e55168e1f108f34312a9597b4450
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-23 05:09:58 +0530

    Added Census Analyser to load Indian States Census Information from a CSV file

---
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -0,0 +1,13 @@
+package censusanalyser;
+
+public class CensusAnalyserException extends Exception {
+    enum ExceptionType{
+        CENSUS_FILE_PROBLEM
+    }
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+}
---
+++ src/main/java/censusanalyser/IndiaStateCodeCSV.java
@@ -0,0 +1,28 @@
+package censusanalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IndiaStateCodeCSV {
+
+    @CsvBindByName(column = "SrNo", required = true)
+    public String SrNo;
+
+    @CsvBindByName(column = "State Name", required = true)
+    public String state;
+
+    @CsvBindByName(column = "TIN", required = true)
+    public String TIN;
+
+    @CsvBindByName(column = "StateCode", required = true)
+    public String stateCode;
+
+    @Override
+    public String toString() {
+        return "IndiaStateCodeCSV{" +
+                "SrNo='" + SrNo + '\'' +
+                ", state='" + state + '\'' +
+                ", TIN='" + TIN + '\'' +
+                ", stateCode='" + stateCode + '\'' +
+                '}';
+    }
+}

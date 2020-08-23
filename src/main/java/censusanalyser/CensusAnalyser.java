f9c47f680e0ec9c539821f774dba298a0c9f7b5e
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-23 05:33:20 +0530

    Added IllegalStateException catch block to check for incorrect header

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -27,8 +27,9 @@ public class CensusAnalyser {
             return numberOfEntries;
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_INCORRECT_HEADER);
         }
-
-
     }
 }
--- src/main/java/censusanalyser/CensusAnalyserException.java
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -2,7 +2,7 @@ package censusanalyser;
 
 public class CensusAnalyserException extends Exception {
     enum ExceptionType{
-        CENSUS_FILE_PROBLEM
+        CENSUS_FILE_PROBLEM, CENSUS_INCORRECT_HEADER
     }
     ExceptionType type;
 
